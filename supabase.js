70125e72a31ec72a3c9950cdd306580de6c2768b
BarthaAttila-hab <atika.76@windowslive.com>
2025-07-06 12:59:53 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,8 @@
-const supabase = supabase.createClient(
-  'https://tzmcofiuqeanftviquwp.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6bWNvZml1cWVhbmZ0dmlxdXdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1ODAzMjYsImV4cCI6MjA2NjE1NjMyNn0.oxD20AplWIl7p6znO0GoVxDWX7aeD_8fhFRmooS2xm4'
-);
+// Itt add meg a saját Supabase projekted URL-jét és public kulcsát!
+const SUPABASE_URL = "https://IDE-ÍRD-BE.supabase.co";
+const SUPABASE_KEY = ;
+
+const supabase = window.supabase
+  ? window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+  : supabase.createClient(SUPABASE_URL, SUPABASE_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6bWNvZml1cWVhbmZ0dmlxdXdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1ODAzMjYsImV4cCI6MjA2NjE1NjMyNn0.oxD20AplWIl7p6znO0GoVxDWX7aeD_8fhFRmooS2xm4KEY);
+
