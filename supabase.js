eb3d4fb06e046231757eeb763179278e6a0764ab
BarthaAttila-hab <atika.76@windowslive.com>
2025-07-10 18:58:08 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://tzmcofiuqeanfvtviquwp.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInN1YiI6InN1cGVyYWRtaW4iLCJpYXQiOjE2ODAwMDAwMDAsImV4cCI6MjAwMDAwMDAwMH0.wJ8U5kbHuN7CPfTfD4-YpW_LkEsnM3BsQ6TRjRU0GrU";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6bWNvZml1cWVhbmZ0dmlxdXdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1ODAzMjYsImV4cCI6MjA2NjE1NjMyNn0.oxD20AplWIl7p6znO0GoVxDWX7aeD_8fhFRmooS2xm4";
 const supabase = window.supabase
   ? window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
   : supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
