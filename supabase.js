f3811f83cf08d59a8bf221c038cbfcef677b915f
BarthaAttila-hab <atika.76@windowslive.com>
2025-07-10 18:00:03 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://tzmcofiuqeanfvtivquwp.supabase.co";
+const SUPABASE_URL = "https://tzmcofiuqeanfvtviquwp.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInN1YiI6InN1cGVyYWRtaW4iLCJpYXQiOjE2ODAwMDAwMDAsImV4cCI6MjAwMDAwMDAwMH0.wJ8U5kbHuN7CPfTfD4-YpW_LkEsnM3BsQ6TRjRU0GrU";
 
 const supabase = window.supabase
